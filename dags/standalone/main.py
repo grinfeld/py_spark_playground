8427fbfc085f3b2e63928dfce622a3a7e81502ae
Michael Grinfeld <grinfeld@gmail.com>
2025-08-09 10:35:58 +0300

    fix standalon

--- dags/standalone/main.py
+++ dags/standalone/main.py
@@ -2,9 +2,6 @@
 Standalone entry point for running the Spark job outside Airflow.
 """
 
-from ..lib.utils import add_project_root_to_path
-add_project_root_to_path()
-
 from ..jobs.spark_job import run_spark_job
 
 
