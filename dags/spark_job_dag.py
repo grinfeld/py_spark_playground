b849dabfded79c1a7d08695d865d423dc55b9732
Michael Grinfeld <grinfeld@gmail.com>
2025-08-24 22:28:42 +0300

    ensure pyspark uses 3.5.6 version and adding packages in spark_job_dag with aws-sdk 2.x.x

--- dags/spark_job_dag.py
+++ dags/spark_job_dag.py
@@ -3,7 +3,7 @@ Airflow 3.0 for Spark job using modern decorators
 """
 import logging
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow.decorators import dag
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from utils.config_manager import config_manager
@@ -15,8 +15,8 @@ spark_master=os.getenv("SPARK_MASTER_URL")
 # Define default arguments for the DAG
 default_args = {
     'owner': 'data-engineering',
-    'retries': 1,
-    'retry_delay': timedelta(minutes=15),
+#    'retries': 1,
+#    'retry_delay': timedelta(minutes=15),
     'email_on_failure': False,
     'email_on_retry': False,
 }
@@ -40,15 +40,17 @@ def spark_job_dag():
     """Airflow 3.0 DAG using task decorators."""
 
     logging.info(os.getcwd())
+    main_file = f"{os.getcwd()}/dags/spark/simple_spark.py"
     spark_submit = SparkSubmitOperator(
         task_id='spark_job_submit',
-        application="/opt/airflow/dags/spark/main.py",
+        application=main_file,
         name='spark_job',
         deploy_mode='client',
-        packages="org.apache.iceberg:iceberg-aws-bundle:1.9.2,"
-                 "org.apache.iceberg:iceberg-hive-runtime:1.7.2,"
-                 "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.9.2,"
-                 "org.apache.hadoop:hadoop-aws:3.3.4"
+        packages="org.apache.hadoop:hadoop-aws:3.3.4"
+                ",software.amazon.awssdk:bundle:2.32.29"
+                # ",org.apache.iceberg:iceberg-aws-bundle:1.9.2"
+                # ",org.apache.iceberg:iceberg-hive-runtime:1.7.2"
+                # ",org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.9.2"
         ,
         conf=configs,
         application_args=[
